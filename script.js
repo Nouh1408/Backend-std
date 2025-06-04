0f11dd8a7ff44cf4b06d0fd4620cce578f1ca5d7
Nouh1408 <ahmedinouh@gmail.com>
2025-06-04 06:58:28 +0300

    fileSystem methods,encoding, high level and low level methods

---
+++ script.js
@@ -0,0 +1,13 @@
+const fs = require("node:fs")
+/**
+ * read
+ * write
+ * create
+ * update
+ * remove
+ * 
+ */
+fs.readFile("./data.json","utf-8",(err,data)=> { //if 
+    console.log(data);
+    
+})
