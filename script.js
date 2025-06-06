370316473f59edc3eb71727c56d4ed014e3ce784
Nouh1408 <ahmedinouh@gmail.com>
2025-06-06 18:00:24 +0300

    writeFile Asynchronous

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const fs = require("node:fs")
 /**
- * read
+ * read (readFileSync - readFile - read)
  * write
  * create
  * update
@@ -15,6 +15,11 @@ const fs = require("node:fs")
     console.log(JSON.parse(data));
     
 }) */
-const data = fs.readFileSync("./data.json", {encoding:"utf-8"})
-console.log(JSON.parse(data));
-
+/* const data = fs.readFileSync("./data.json", {encoding:"utf-8"} )
+console.log(JSON.parse(data)); */
+let Fname = "Ahmed"
+let Lname = "Nouh"
+fs.writeFile("./data.txt", ` Hello i am ${Fname} ${Lname}`,{flag:"a"},(err)=>{  //teh HelloWorld is the data not sent as a parameter unlike readFile
+    err && console.log(err.message);
+    
+})
