8fd425a1480c0c2c276545d42785b2d85966db1b
Nouh1408 <ahmedinouh@gmail.com>
2025-06-06 18:34:01 +0300

    writefile

--- script.js
+++ script.js
@@ -17,9 +17,23 @@ const fs = require("node:fs")
 }) */
 /* const data = fs.readFileSync("./data.json", {encoding:"utf-8"} )
 console.log(JSON.parse(data)); */
-let Fname = "Ahmed"
+/* let Fname = "Ahmed"
 let Lname = "Nouh"
 fs.writeFile("./data.txt", ` Hello i am ${Fname} ${Lname}`,{flag:"a"},(err)=>{  //teh HelloWorld is the data not sent as a parameter unlike readFile
     err && console.log(err.message);
     
 })
+ */
+try{
+    let oldData = fs.readFileSync("./data.json", {encoding:"utf-8"})
+    let newData = {name:"Ahmed", salary:30000}
+    oldData = JSON.parse(oldData)
+    oldData.push(newData)
+    
+    fs.writeFileSync("./data.json", JSON.stringify(oldData)) //{flag:"a"}not prefered as it brings the new data beside array in JSON file
+} catch(err){
+    console.error(err.message);
+    
+}
+
+
