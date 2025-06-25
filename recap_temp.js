316b65deeef06e4d325f9c1ef5d24bb74f18f365
Nouh1408 <ahmedinouh@gmail.com>
2025-06-25 20:47:24 +0300

    loopss

---
+++ recap_temp.js
@@ -0,0 +1,46 @@
+//simple for loop
+
+/* let arr=["Messi", "ronaldo","Salah"]
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+    
+} */
+
+//forin
+//fprin works with array and objects
+/* const arr =[10,20,30,40,50] //with array
+for(const i in arr){
+    console.log(arr[i]);
+} */
+/* const user ={name:"Ahmed",age:21} //with object
+for(const key in user){
+    console.log(user[key]);
+} */
+
+//for of with array
+const str = "basma";
+for (const i of str) {
+    console.log(i);
+}
+
+
+
+
+
+
+
+
+//--------------------Questions-------------------
+//forin when working with array is it the same functions as dor loop
+/**
+ * 
+ *  let arr=["Messi", "ronaldo","Salah"]
+ * for(let i=0;i<arr.length;i++)
+ * {console.log(arr[i]);
+ *  
+ * const arr =[10,20,30,40,50]
+ * for(const i in arr){    
+ * console.log(arr[i]); 
+ * }  
+ * 
+ */
