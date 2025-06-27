ab26688a8a0b09ddac1937b6a9e70eb1c6f14bca
Nouh1408 <ahmedinouh@gmail.com>
2025-06-28 02:02:59 +0300

    sda

--- query.sql
+++ query.sql
@@ -2,15 +2,19 @@
 -- DML data manipulation language responsible for changing data insert, update, delete, slect
 --dql data query language select
 -- dcl data control language --> privelages, grant , revoke
- -- TCL transaction control language  commit, rollback, savepoint
- CREATE DATABASE IF NOT EXISTS 'uber';
- use 'uber';
- create table 'drivers'(
-    id int(11) NOT null AUTO_INCREMENT primary key,
-    NAME VARCHAR(50) NOT null,
-    email VARCHAR(50) NOT null,
-    status ENUM('active', 'inactive', 'suspended') not null default 'active', --must be small letters 
-    dob DATE,
-    createdAt timestamp default current_timestamp,
-    updateAt timestamp default current_timestamp on update current_timestamp,
- )
+-- TCL transaction control language  commit, rollback, savepoint
+CREATE DATABASE IF NOT EXISTS 'uber';
+
+use 'uber';
+
+create table
+   'drivers' (
+      id int (11) NOT null AUTO_INCREMENT primary key,
+      NAME VARCHAR(50) NOT null,
+      email VARCHAR(50) NOT null,
+      status ENUM ('active', 'inactive', 'suspended') not null default 'active', --must be small letters 
+      dob DATE,
+      createdAt timestamp default current_timestamp,
+      updateAt timestamp default current_timestamp on update current_timestamp,
+   )
+alter table 'drivers' add subscription enum('normal', 'premium') not null default 'normal' after status
