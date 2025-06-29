92809bf422d7a3683cd4e9b1cffcda00236f0687
Nouh1408 <ahmedinouh@gmail.com>
2025-06-29 16:40:46 +0300

    created table cars adn drivers

--- query.sql
+++ query.sql
@@ -3,18 +3,22 @@
 --dql data query language select
 -- dcl data control language --> privelages, grant , revoke
 -- TCL transaction control language  commit, rollback, savepoint
-CREATE DATABASE IF NOT EXISTS 'uber';
-
-use 'uber';
-
-create table
-   'drivers' (
-      id int (11) NOT null AUTO_INCREMENT primary key,
-      NAME VARCHAR(50) NOT null,
-      email VARCHAR(50) NOT null,
-      status ENUM ('active', 'inactive', 'suspended') not null default 'active', --must be small letters 
-      dob DATE,
-      createdAt timestamp default current_timestamp,
-      updateAt timestamp default current_timestamp on update current_timestamp,
-   )
-alter table 'drivers' add subscription enum('normal', 'premium') not null default 'normal' after status
+create database `uber`;
+create table 'drivers'(
+   id    int   not null auto_increment,
+   name  varchar(50) not null,
+   email varchar(100) not null,
+   password varchar(100) mot null,
+   status enum('online', 'offline') default 'offline',
+   createdAT timestamp default current_timestamp 
+   updatedAT timestamp default current_timestamp on update current_timestamp
+);
+create table  `car`(
+   id int not null auto_increment primary key,
+   model varchar(50) not null,
+   numberOfLicsence varchar(8) not null unique,
+   driverID int not null unique,
+   createdAt timestamp default current_timestamp,
+   updatedat timestamp default current_timestamp on update current_timestamp,
+   foreign key (driverID) refrence drivers(id)
+);
